a38ebb0a7585358a2a690e635f66f6ace8e802dc
Kiranpreet Kaur <kau15125@sheridancollege.ca>
2024-02-23 17:16:50 -0500

    Commited

---
+++ src/TestUserprofile.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kiran
+ */
+import java.util.Scanner;
+
+
+public class TestUserprofile {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = input.nextLine();
+
+        System.out.println("Enter your favorite actor:");
+        String favoriteActor = input.nextLine();
+
+        System.out.println("Your userProfile was created.");
+    }
+}
+
+    
+
